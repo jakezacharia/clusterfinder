d59a4f70cf955003c706c23ebea385f483e8cd0c
Jake <jz.exe@proton.me>
2024-09-07 11:45:07 -0700

    improved test case functionality, added <= check for circles intersecting only at one point

--- cluster.py
+++ cluster.py
@@ -17,7 +17,7 @@ def distance_check(currentCircle: Circle, circles: list, visited: set):
     for c in circles:
         if c not in visited:
             dist = math.sqrt((c.x - currentCircle.x)**2 + (c.y - currentCircle.y)**2)
-            if dist < currentCircle.r + c.r:
+            if dist <= currentCircle.r + c.r:
                 # check if one circle is within another
                 if dist + min(currentCircle.r, c.r) < max(currentCircle.r, c.r):
                     continue
@@ -40,35 +40,23 @@ def cluster_detect(circles):
     return False
 
 def main():
-  # test case 1
-  case1 = [Circle(1,3,0.7), Circle(2,3,0.4), Circle(3,3,0.9)]
-  print("\nTEST CASE 1")
-  print(str(case1[0]), str(case1[1]), str(case1[2]))
-  cluster_detect(case1)
 
-  # test case 2
-  case2 = [Circle(1.5,1.5,1.3), Circle(4,4,0.7)]
-  print("TEST CASE 2")
-  print(str(case2[0]), str(case2[1]))
-  cluster_detect(case2)
+  test_cases = [
+  [Circle(1,3,0.7), Circle(2,3,0.4), Circle(3,3,0.9)],
+  [Circle(1.5,1.5,1.3), Circle(4,4,0.7)],
+  [Circle(0.5,0.5,0.5), Circle(1.5,1.5,1.1), Circle(0.7,0.7,0.4), Circle(4,4,0.7)],
+  [Circle(2,2,5), Circle(2,2,3)],
+  [Circle(3,1,4), Circle(2,4,0.9), Circle(2,1.5,5)],
+  [Circle(1,1,1), Circle(3,1,1), Circle(5,1,1), Circle(7,1,1)]
+  ]
 
-  # test case 3
-  case3 = [Circle(0.5,0.5,0.5), Circle(1.5,1.5,1.1), Circle(0.7,0.7,0.4), Circle(4,4,0.7)]
-  print("TEST CASE 3")
-  print(str(case3[0]), str(case3[1]), str(case3[2]), str(case3[3]))
-  cluster_detect(case3)
 
-  # test case 4 (circle within a circle, should return false)
-  case4 = [Circle(2,2,5), Circle(2,2,3)]
-  print("TEST CASE 4 - Circle within a circle : expected result FALSE")
-  print(str(case4[0]), str(case4[1]))
-  cluster_detect(case4)
+  for i, case in enumerate(test_cases, start=1):
+      print(f"TEST CASE {i}")
+      for circle in case:
+          print(str(circle))
+      cluster_detect(case)
 
-  # test case 5 (out of order version)
-  case5 = [Circle(3,1,4), Circle(2,4,0.9), Circle(2,1.5,5)]
-  print("TEST CASE 5 - Circles out of sequence, still connected : expected result TRUE")
-  print(str(case5[0]), str(case5[1]), str(case5[2]))
-  cluster_detect(case5)
 
 if __name__ == "__main__":
   main()
